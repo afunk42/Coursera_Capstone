71801a76f9888cfbbc71680f1c3d8620e871f270
afunk42 <78036124+afunk42@users.noreply.github.com>
2021-03-11 19:09:50 -0500

    Add files via upload

---
+++ Coursera_Capstone_Week 1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Coursera_Capstone_Week 1
+
+# ## This notbook will be used for the creation and execution of the Coursera Applied Data Science Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[5]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
